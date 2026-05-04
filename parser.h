1fced4d4692f781c5f8f6753c03fe279ebd81f29
Christopher Bazley <cs99cjb@gmail.com>
2026-05-04 12:00:23 +0100

    Add fallback definition of _Optional

--- parser.h
+++ parser.h
@@ -8,15 +8,19 @@
 #define PARSER_H
 
 /* ISO C library headers */
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 /* StreamLib headers */
 #include "Reader.h"
 
-bool apoc_to_obj(Reader *in, _Optional FILE *out,
-                 const int first, const int last, _Optional const char *name,
-                 const long int index_offset,
-                 const char *mtl_file, const unsigned int flags);
+#if !defined(USE_OPTIONAL) && !defined(_Optional)
+#define _Optional
+#endif
+
+bool apoc_to_obj(Reader *in, _Optional FILE *out, const int first,
+                 const int last, _Optional const char *name,
+                 const long int index_offset, const char *mtl_file,
+                 const unsigned int flags);
 
 #endif /* PARSER_H */
